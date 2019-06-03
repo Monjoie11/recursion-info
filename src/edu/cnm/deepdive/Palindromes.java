f1a52bca3c38e9b2039616947bef4395940b3c3f
Monjoie11 <jeff.franken@gmail.com>
2019-06-03 10:57:53 -0600

    added test and the wisdom of saint Asher

--- src/edu/cnm/deepdive/Palindromes.java
+++ src/edu/cnm/deepdive/Palindromes.java
@@ -2,25 +2,26 @@ package edu.cnm.deepdive;
 
 public class Palindromes {
 
-  static String s = "A man, a plan, a canal, Panama!";
-  static String s2;
 
 
-  public static boolean isPalindrome() {
-    s2 = s.replaceAll("\\W", "");
+
+  public static boolean isPalindrome(String s) {
+    String s2 = s.replaceAll("\\W", "");
     s2 = s2.toLowerCase();
     char[] chars = s2.toCharArray();
     for (int i = 0, j = chars.length - 1; i < j; i++, j--) {
       if (chars[i] != chars[j]) {
+        System.out.println(s2);
+        System.out.println(false);
         return false;
       }
     }
+    System.out.println(true);
     return true;
   }
 
   public static void main(String[] args) {
-    System.out.println(isPalindrome());
-    System.out.println(s2);
-  }
+
+}
 
 }
